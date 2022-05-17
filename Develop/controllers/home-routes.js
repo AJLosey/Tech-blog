55de153561c8997e6cec894a71f5d2824b08a0bb
AJLosey <aaronlosey11@gmail.com>
2022-05-16 21:32:00 -0500

    posts are retrieved

--- Develop/controllers/home-routes.js
+++ Develop/controllers/home-routes.js
@@ -5,9 +5,12 @@ const { Post, Comment, User } = require('../models/');
 router.get('/', async (req, res) => {
 
   try {
-    const posts = await Post.findAll();
-    res.render('all-posts', {
+    const postData = await Post.findAll();
 
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('all-posts', {
+      posts
     })
   } catch (err) {
     console.log(err);
