552632a2ae587492aeb077edf655c450d98e2773
AJLosey <aaronlosey11@gmail.com>
2022-05-16 20:51:41 -0500

    sign up works

--- Develop/public/js/signup.js
+++ Develop/public/js/signup.js
@@ -1,21 +1,28 @@
-const signupFormHandler = async function(event) {
+const signupFormHandler = async function (event) {
   event.preventDefault();
 
-  const usernameEl = document.querySelector('#username-input-signup');
-  const passwordEl = document.querySelector('#password-input-signup');
+  const usernameEl = document.querySelector('#username-input-signup').value;
+  const passwordEl = document.querySelector('#password-input-signup').value;
 
-  const response = await fetch('', {
-    
-    }),
-    headers: { 'Content-Type': 'application/json' },
-  });
+  if (usernameEl && passwordEl) {
+    const response = await fetch('/api/user', {
+      method: 'POST',
+      body: JSON.stringify({ username: usernameEl, password: passwordEl }),
+      headers: { 'Content-Type': 'application/json' },
+    })
 
-  if (response.ok) {
-    document.location.replace('/dashboard');
-  } else {
-    alert('Failed to sign up');
+
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to sign up');
+    }
   }
 };
+
+
+
 
 document
   .querySelector('#signup-form')
